d8ff5d53d5f2c79edcc1251224f9bf93da23b74d
Ciro S. Costa <utxobr@protonmail.com>
2021-10-07 18:59:53 +0000

    bump
    Signed-off-by: Ciro S. Costa <utxobr@protonmail.com>

--- hack/publish.sh
+++ hack/publish.sh
@@ -2,6 +2,35 @@
 
 set -o errexit
 
-cat <(echo -e '#@data/values\n---') <(gyr ./config/values.yaml) |
-	ytt --ignore-unknown-comments -f- -f ./config/images.yaml |
-	kbld --images-annotation=false --build-concurrency=1 -f- > ./images.yaml
+main() {
+        case $1 in
+        config)
+		generate_build_config
+		;;
+
+        build)
+		build
+		;;
+
+        *)
+                echo "usage: $0 (build|config)"
+                exit 1
+                ;;
+
+        esac
+}
+
+generate_build_config() {
+        cat <(echo -e '#@data/values\n---') <(gyr ./config/values.yaml) |
+                ytt --ignore-unknown-comments -f- -f ./config/images.yaml > \
+                        ./build-config.yaml
+}
+
+build() {
+        kbld -f ./build-config.yaml \
+                --images-annotation=false --build-concurrency=1 > \
+                ./images.yaml
+
+}
+
+main "$@"
